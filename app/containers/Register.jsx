2c4632aea4cfec93768fafca154bab921166a8f3
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-11-23 18:08:29 -0800

    Updated redux-form

--- app/containers/Register.jsx
+++ app/containers/Register.jsx
@@ -1,9 +1,11 @@
-import React, {Component, PropTypes} from 'react';
-import {reduxForm} from 'redux-form';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 import GoogleLogin from '../containers/GoogleLogin';
-import {signUp, toggleLoginMode} from '../actions/users';
+import FormField from '../components/FormField';
+import { signUp, toggleLoginMode } from '../actions/users';
 
-const validate = values => {
+const validate = (values) => {
   const errors = {};
   if (!values.email) {
     errors.email = 'Required';
@@ -19,88 +21,49 @@ const validate = values => {
   return errors;
 };
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
-  submit() {
-    const {signUp, fields: { email, password, name } } = this.props;
-    signUp({
-        name: name.value,
-        email: email.value,
-        password: password.value
-    }, 'register');
-  }
-  render() {
-    const { fields: { email, password, name }, error, resetForm, handleSubmit, submitting, toggleLoginMode } = this.props;
-    return (
-      <div>
-        <div className="text-center">
-          <h2>Register with Email</h2>
-          <div>
-            Already have an account? &nbsp;
-            <a onClick={toggleLoginMode}>
-              Login Here
-            </a>
-          </div>
+let Register = ({ error, handleSubmit, submitting, toggleLoginMode, signUp }) => {
+  return (
+    <div>
+      <div className="text-center">
+        <h2>Register with Email</h2>
+        <div>
+          Already have an account? &nbsp;
+          <a tabIndex="0" onClick={toggleLoginMode}>
+            Login Here
+          </a>
         </div>
-        <form className="form-horizontal" onSubmit={handleSubmit(this.submit)}>
-          <div className={'form-group' + (name.touched && name.error ? ' has-error' : '')}>
-            <label htmlFor="nameInput" className="col-sm-2 control-label">Name</label>
-            <div className={'col-sm-' + (name.touched && name.error ? '5' : '8')}>
-              <input id="nameInput" type="text" className="col-sm-8 form-control" placeholder="Name" {...name} />
-            </div>
-            {name.touched && name.error && <div className="col-sm-3 help-block">{name.error}</div>}
-          </div>
-          <div className={'form-group' + (email.touched && email.error ? ' has-error' : '')}>
-            <label htmlFor="emailInput" className="col-sm-2 control-label">Email</label>
-            <div className={'col-sm-' + (email.touched && email.error ? '5' : '8')}>
-              <input id="emailInput" type="text" className="col-sm-8 form-control" placeholder="Email" {...email} />
-            </div>
-            {email.touched && email.error && <div className="col-sm-3 help-block">{email.error}</div>}
-          </div>
-          <div className={'form-group' + (password.touched && password.error ? ' has-error' : '')}>
-            <label htmlFor="pwInput" className="col-sm-2 control-label">Password</label>
-            <div className={'col-sm-' + (password.touched && password.error ? '5' : '8')}>
-              <input id="pwInput" type="password" className="col-sm-8 form-control" placeholder="Password" {...password} />
-            </div>
-            {password.touched && password.error && <div className="col-sm-3 help-block">{password.error}</div>}
-          </div>
-          <div className="text-center">
-            {error && <p className="bg-danger">{error}</p>}
-            <button type="submit" className="btn btn-primary btn-lg" style={{ margin: 10 }} disabled={submitting}>
-              {submitting ? <i className="fa fa-circle-o-notch fa-spin" /> : <i className="fa fa-pencil-square-o" />} Register
-            </button>
-            <button type="button" className="btn btn-default btn-lg" style={{ margin: 10 }} disabled={submitting} onClick={resetForm}>
-              Clear Values
-            </button>
-          </div>
-          <hr />
-          <GoogleLogin />
-        </form>
       </div>
-    );
-  }
-}
+      <form className="form-horizontal" onSubmit={handleSubmit(values => signUp(values, 'register'))}>
+        <Field name="name" type="name" component={FormField} label="Name" />
+        <Field name="email" type="email" component={FormField} label="Email" />
+        <Field name="password" type="password" component={FormField} label="Password" />
+        <div className="text-center">
+          {error && <p className="bg-danger">{error}</p>}
+          <button type="submit" className="btn btn-primary btn-lg" style={{ margin: 10 }} disabled={submitting}>
+            {submitting ? <i className="fa fa-circle-o-notch fa-spin" /> : <i className="fa fa-pencil-square-o" />} Register
+          </button>
+        </div>
+        <hr />
+        <GoogleLogin />
+      </form>
+    </div>
+  );
+};
 
 Register.propTypes = {
   signUp: PropTypes.func.isRequired,
   toggleLoginMode: PropTypes.func.isRequired,
-  fields: PropTypes.object.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   error: PropTypes.string,
-  resetForm: PropTypes.func.isRequired,
   submitting: PropTypes.bool.isRequired
 };
 
-const formConfig = {
+Register = reduxForm({
   form: 'register',
   fields: [
     'email', 'password', 'name'
   ],
   validate
-};
+})(Register);
 
-// reduxForm connects redux store/form to app
-export default reduxForm(formConfig, null, {signUp, toggleLoginMode})(Register);
+export default connect(null, { signUp, toggleLoginMode })(Register);
