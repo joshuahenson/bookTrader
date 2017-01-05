341a95e4e5ba6af4dcec9f401d9dd05d27a131db
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-01-05 09:37:06 -0800

    Replaced placeholder text

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Main = () => {
   return (
-    <p>Placeholder text</p>
+    <div className="book-bg">
+      <img src="https://source.unsplash.com/IOzk8YKDhYg/1500x1500" role="presentation" />
+      <div className="well">
+        <div className="page-header text-center">
+          <h1>Book Trader <small>Demo App</small></h1>
+        </div>
+        <div className="row">
+          <div className="col-sm-offset-2 col-sm-8">
+            <h4>
+              A place for sharing books.
+            </h4>
+            <ul className="list-unstyled">
+              <li>List your books online</li>
+              <li>Browse other user&apos;s books</li>
+              <li>Make a trade</li>
+            </ul>
+            <Link to="/add_book">Get Started Now</Link>
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 };
 
